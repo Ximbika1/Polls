4c327ad564605ece4fb09b8652bd93544cec22ee
Pedro Augusto <pedrocanuto2002@gmail.com>
2025-07-24 11:31:54 -0300

    ajustando schema e criando a primeira enquete

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,29 @@
 import fastify from 'fastify'
+import {z} from 'zod'
+import { PrismaClient } from '@prisma/client';
+
+// GET, POST, PUT(alteracao), DELETE, PATCH(alteracao de um campo especifico dentro de um recurso), HEAD, OPTIONS
 
 const app = fastify()
 
-app.get('/hello', () => {
-    return 'Hello NLW'
+const prisma = new PrismaClient()
+
+app.post('/polls', async (request) => {
+    const createPollBody = z.object({
+        title: z.string()
+    })
+
+    const { title } = createPollBody.parse(request.body)
+
+    const poll = await prisma.poll.create({
+        data: {
+            title,
+        }
+    })
+
+    console.log(title)
+
+    return poll
 })
 
 app.listen({ port: 3333}).then(() => {
