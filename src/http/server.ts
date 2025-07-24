9752523af1388a3e48d66b4b820bc3c821adf79f
Pedro Augusto <pedrocanuto2002@gmail.com>
2025-07-24 13:19:13 -0300

    status(201)

--- src/http/server.ts
+++ src/http/server.ts
@@ -8,7 +8,7 @@ const app = fastify()
 
 const prisma = new PrismaClient()
 
-app.post('/polls', async (request) => {
+app.post('/polls', async (request, reply) => {
     const createPollBody = z.object({
         title: z.string()
     })
@@ -21,9 +21,7 @@ app.post('/polls', async (request) => {
         }
     })
 
-    console.log(title)
-
-    return poll
+    return reply.status(201).send({pollId: poll.id})
 })
 
 app.listen({ port: 3333}).then(() => {
